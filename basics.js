0628a0f77987605dfc7f5d8cbf3afe30667e4502
Mobcoder ID-100 <mobcoderid-100@Mobcoders-Mac-mini.local>
2026-06-05 17:30:40 +0530

    js tutorial day4

--- basics.js
+++ basics.js
@@ -105,12 +105,50 @@
 //     alert(err);
 // }
 
-let promise= new Promise(function(resolve, reject){
-    setTimeout(()=>resolve("Done"),2000);
-})
+// let promise= new Promise(function(resolve, reject){
+//     setTimeout(()=>resolve("Done"),2000);
+// })
 
-promise.then(
-    result=> alert(result),
-    error=>alert(error)
-);
+// promise.then(
+//     result=> alert(result),
+//     error=>alert(error)
+// );
 
+// Promise.all([
+//     new Promise(resolve=>setTimeout(()=>resolve(1),3000)),
+//     new Promise(resolve=>setTimeout(()=>resolve(2),2000)),
+//     new Promise(resolve=>setTimeout(()=>resolve(3),1000))
+// ]).then(alert)
+
+// let urls=[
+//     'https://github.com/kandarp-05',
+//     'https://www.w3schools.com/js/DEFAULT.asp',
+//     'https://javascript.info/'
+// ];
+// Promise.allSettled(urls.map(url=>fetch(url))).then(results=>{
+//     results.forEach((result, num)=>{
+//         if (result.status=='fulfilled'){
+//             alert(`${urls[num]}: ${result.value.status}`);
+//         }
+//         if (result.status=='rejected'){
+//             alert(`${urls[num]}: ${result.reason}`);
+//         }
+//     });
+// });
+
+// async function f(){
+//     let promise=new Promise((resolve,reject)=>{
+//         setTimeout(()=> resolve("done"),1000);
+//     })
+//     let result=await promise;
+//     alert(result);
+// }
+// f();
+async function wait(){
+    await new Promise(resolve=> setTimeout(resolve,1000 ));
+    return 10;
+}
+function f(){
+    wait().then(result=>alert(result))
+}
+f();
